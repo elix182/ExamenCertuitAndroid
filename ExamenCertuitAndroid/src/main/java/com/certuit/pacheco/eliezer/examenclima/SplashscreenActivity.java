a6cabecf41ac1c466ac83ca7f15dec614d33c3ed
Eliezer Pacheco Jiménez <elix-13@hotmail.com>
2019-01-31 13:21:47 -0800

    Agregar dependencias a proyecto

--- ExamenCertuitAndroid/src/main/java/com/certuit/pacheco/eliezer/examenclima/SplashscreenActivity.java
+++ ExamenCertuitAndroid/src/main/java/com/certuit/pacheco/eliezer/examenclima/SplashscreenActivity.java
@@ -3,11 +3,11 @@ package com.certuit.pacheco.eliezer.examenclima;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashscreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splashscreen);
     }
 }
