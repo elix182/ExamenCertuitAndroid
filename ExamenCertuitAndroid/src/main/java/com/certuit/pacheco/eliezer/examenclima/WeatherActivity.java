91ce3e1dfa271ae31e958fc7a26b51293c43b9eb
Eliezer Pacheco Jiménez <elix-13@hotmail.com>
2019-01-31 22:56:37 -0800

    Agregar navigation bar

--- ExamenCertuitAndroid/src/main/java/com/certuit/pacheco/eliezer/examenclima/WeatherActivity.java
+++ ExamenCertuitAndroid/src/main/java/com/certuit/pacheco/eliezer/examenclima/WeatherActivity.java
@@ -1,7 +1,16 @@
 package com.certuit.pacheco.eliezer.examenclima;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class WeatherActivity extends AppCompatActivity {
 
@@ -9,5 +18,57 @@ public class WeatherActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_weather);
+    setupToolbar();
+  }
+
+  @Override
+  public void onBackPressed() {
+    DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+    if(drawerLayout.isDrawerOpen(Gravity.START)) {
+      drawerLayout.closeDrawer(Gravity.START);
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+    switch(item.getItemId()) {
+      case android.R.id.home:
+        drawerLayout.openDrawer(Gravity.START);
+        break;
+    }
+    return super.onOptionsItemSelected(item);
+  }
+
+  private void setupToolbar(){
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    toolbar.setNavigationIcon(R.drawable.baseline_menu_white_24);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    setupNavigationMenu();
+  }
+
+  private void setupNavigationMenu(){
+    final DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+    NavigationView listView = findViewById(R.id.left_sidebar);
+    listView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+      @Override
+      public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch(menuItem.getItemId()){
+          case R.id.favorite_menu_button:
+            Toast.makeText(getApplicationContext(),"Favoritos",Toast.LENGTH_SHORT).show();
+            drawerLayout.closeDrawer(Gravity.START);
+            break;
+          case R.id.search_menu_button:
+            Toast.makeText(getApplicationContext(),"Buscar",Toast.LENGTH_SHORT).show();
+            drawerLayout.closeDrawer(Gravity.START);
+            break;
+          default: break;
+        }
+        return true;
+      }
+    });
   }
 }
